cd7e33df3b030aa2d4f4ccf5716faa30240c8864
maithenkumar <maitheen1905@gmail.com>
2023-10-01 21:39:46 +0530

    intizile

---
+++ src/main/java/com/maitheen/authuapi/AuthuApiApplication.java
@@ -0,0 +1,13 @@
+package com.maitheen.authuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maitheen/authuapi/AuthuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maitheen.authuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
